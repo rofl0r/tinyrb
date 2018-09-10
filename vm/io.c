d7f058b9bc2729b3368ab9ebefa54ec9f5284f5b
Kirill Timofeev <kirill.timofeev@hulu.com>
2018-09-10 14:03:40 -0700

    IO.read() method added

--- vm/io.c
+++ vm/io.c
@@ -1,5 +1,8 @@
 #include "tr.h"
 #include "internal.h"
+#include "utils.h"
+
+#include <sys/stat.h>
 
 OBJ TrIO_new(VM) {
   TrIO *io = TR_INIT_CORE_OBJECT(IO);
@@ -12,8 +15,26 @@ OBJ TrIO_close(VM, OBJ self) {
   return TR_NIL;
 }
 
+OBJ TrIO_read(VM, OBJ self, OBJ path_arg) {
+  UNUSED(vm);
+  UNUSED(self);
+  char *path = TR_STR_PTR(path_arg);
+  size_t str_len;
+
+  char *str = read_file_into_string(path, &str_len);
+  if (str != NULL) {
+    TrString *s = TR_INIT_CORE_OBJECT(String);
+    s->len = str_len;
+    s->interned = 0;
+    s->ptr = str;
+    return (OBJ)s;
+  }
+  tr_raise_errno(path);
+  return TR_NIL;
+}
 
 void TrIO_init(VM) {
   OBJ c = TR_INIT_CORE_CLASS(IO, Object);
   tr_def(c, "close", TrIO_close, 0);
+  tr_metadef(c, "read", TrIO_read, 1);
 }
